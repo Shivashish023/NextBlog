346600673b7de62324de4c78b204f7651cb31c1a
Rival <shivashish.saklani@gmail.com>
2025-03-25 00:02:03 +0530

    createfix

--- src/app/api/createroute/route.ts
+++ src/app/api/createroute/route.ts
@@ -4,23 +4,29 @@ import UserModel from "@/models/UserModel";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-    await dbConnect(); // Connect to MongoDB
+    await dbConnect();
 
     try {
-        const { title, content, clerkId } = await req.json(); 
+        const { title, content, clerkId } = await req.json();
 
         if (!clerkId) {
             return NextResponse.json({ message: "User not authenticated" }, { status: 401 });
         }
 
-        
+     
         const user = await UserModel.findOne({ clerkId });
 
         if (!user) {
             return NextResponse.json({ message: "User not found" }, { status: 404 });
         }
 
-        const newBlog = new Blog({ title, content, userId: user._id });
+        const newBlog = new Blog({
+            title,
+            content,
+            userId: user._id,
+            clerkId, 
+        });
+
         await newBlog.save();
 
         user.blogs.push(newBlog._id);
