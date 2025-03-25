6509b2960cecebef286bf5732311c90e9cbce198
Rival <shivashish.saklani@gmail.com>
2025-03-25 18:55:12 +0530

    homefix

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/Navbar/page";
+
 import dbConnect from "@/utils/dbConnect";
 import UserModel from "@/models/UserModel";
 import { currentUser } from "@clerk/nextjs/server";
@@ -8,32 +8,30 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
   await dbConnect();
 
   const user = await currentUser();
-  if (!user) return null; 
 
-  try {
-    
-    let loggedInUser = await UserModel.findOne({ clerkId: user.id });
+  if (user) {
+    try {
+      let loggedInUser = await UserModel.findOne({ clerkId: user.id });
 
-  
-    if (!loggedInUser) {
-      loggedInUser = new UserModel({
-        clerkId: user.id,
-        email: user.emailAddresses[0].emailAddress,
-        name: user.fullName || "Unknown",
-        blogs:[]
-      });
+      if (!loggedInUser) {
+        loggedInUser = new UserModel({
+          clerkId: user.id,
+          email: user.emailAddresses[0].emailAddress,
+          name: user.fullName || "Unknown",
+          blogs: [],
+        });
 
-      await loggedInUser.save();
+        await loggedInUser.save();
+      }
+    } catch (error) {
+      console.error("Error handling user login:", error);
     }
-  } catch (error) {
-    console.error("Error handling user login:", error);
-    return null; 
   }
 
   return (
     <div>
-      
-      {children}
+   
+      {children} 
     </div>
   );
 };
